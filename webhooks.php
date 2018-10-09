5c23b2f062f79c6b1b7341591f1ef34d3f93ec4e
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-09 14:04:29 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -7,6 +7,7 @@ $access_token = '2/ktZPikCIG3ZL076jYYNfe3mAg5TQ55Uz/GoA6dILTTUClIvRILXa/o96Lp8gf
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
+var_dump($events);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 // Loop through each event
@@ -45,6 +46,6 @@ echo $result . "\r\n";
 }
 echo "OK";
 echo "ok very good";
-var_dump($event);
-echo $result;
+//var_dump($event);
+//echo $result;
 
