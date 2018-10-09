c37f19f8172d33276fd5143f77d845c76871c66e
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-09 11:49:19 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -15,7 +15,7 @@ foreach ($events['events'] as $event) {
 if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 // Get text sent
 $text = $event['source']['userId'];
-print_r($event)
+print_r($event);
 // Get replyToken
 $replyToken = $event['replyToken'];
 // Build message to reply back
