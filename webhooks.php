120f6148ba330c6c2dd77dd5bc59a347552a67be
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-10 12:34:37 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,5 +1,5 @@
 <?php // callback.php
-http://fixie:bzgCCzMBFxQGvPd@velodrome.usefixie.com:80);
+curl http://welcome.usefixie.com --proxy http://fixie:bzgCCzMBFxQGvPd@velodrome.usefixie.com:80
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 $access_token = '2/ktZPikCIG3ZL076jYYNfe3mAg5TQ55Uz/GoA6dILTTUClIvRILXa/o96Lp8gfcLH5q2dxJeVtiHq8NZ5mNCHtH8VBUKzKfNBxIpMKZnn1XFCTFp/pTOwydGaGPvP0eFvZDsizHoFIRbWFTqYowyQdB04t89/1O/w1cDnyilFU=';
