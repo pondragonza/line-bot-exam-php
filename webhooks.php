8b6fc107ea5a4a5c5124a1dc6968a068216d3dbd
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-11 11:45:04 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -18,7 +18,7 @@ if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 // Get text sent
 $text = $event['source']['userId'];
 //$text = $event['source'] . " " . $event['userId'];
-echo($text);
+echo "$text";
 //var_dump($event);
 // Get replyToken
 $replyToken = $event['replyToken'];
