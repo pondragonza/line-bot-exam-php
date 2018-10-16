b8304cb669d47ae9e0695013ae51378a6f254370
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-16 12:18:52 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -22,8 +22,8 @@ $replyToken = $event['replyToken'];
 // Build message to reply back
 $messages = [
 'userId' => 'userId', 
-'type' => 'follow',
-'follow' => $text
+'type' => 'text',
+'text' => $text
 ];
 // Make a POST Request to Messaging API to reply to sender
 $url = 'https://api.line.me/v2/bot/message/reply';
