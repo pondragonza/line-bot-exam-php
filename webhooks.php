173528ad36d84a549983957f3a787ff825ddd71b
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-16 13:02:38 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -14,7 +14,7 @@ foreach ($events['events'] as $event) {
 // Reply only when message sent is in 'text' format
 if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 // Get text sent
-$text = $event['source'] . "                  " . $event['type'] . "                     " . $event['userId'];
+$text = $event['source'] . " " . $event['type,userId'];
 //$text = $event['source'] . " " . $event['userId'];
 echo ($text);
 // Get replyToken
