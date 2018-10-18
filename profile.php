864ff21e37f932f8c015b0730e315655e42406c1
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-18 13:11:46 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -2,18 +2,17 @@
 
 
 $access_token = '2/ktZPikCIG3ZL076jYYNfe3mAg5TQ55Uz/GoA6dILTTUClIvRILXa/o96Lp8gfcLH5q2dxJeVtiHq8NZ5mNCHtH8VBUKzKfNBxIpMKZnn1XFCTFp/pTOwydGaGPvP0eFvZDsizHoFIRbWFTqYowyQdB04t89/1O/w1cDnyilFU=';
+
 $userId = 'U440df8f5d0462b99d170bf9effbcd26a';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
+
 $headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-
 $result = curl_exec($ch);
 curl_close($ch);
 
