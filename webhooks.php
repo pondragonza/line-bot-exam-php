e702606de76936c90077a96459539b70caf818e3
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-18 11:17:42 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -15,16 +15,16 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 // Loop through each event
 foreach ($events['events'] as $event) {
-fwrite($file , var_export($events['events'],true));
-fclose($file);
+//fwrite($file , var_export($events['events'],true));
+//fclose($file);
 // Reply only when message sent is in 'text' format
-if ($event['type'] == 'follow'); //&& $event['message']['type'] == 'text') {
+if ($event['type'] == 'follow'){ //&& $event['message']['type'] == 'text') {
   echo "1";
 // Get text sent
 $text = $event['source']['userId'];
 // Get replyToken
-  //fwrite($file , var_export($text,true));
-  //fclose($file);
+  fwrite($file , var_export($text,true));
+  fclose($file);
 $replyToken = $event['replyToken'];
 // Build message to reply back
 $messages = [
