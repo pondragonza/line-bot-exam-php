d8f3da4bd373cf4d01a45fa1d0802bab8115139a
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-18 10:15:43 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -4,8 +4,8 @@ require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 //$access_token = 'XXXXXXXXX';
 $access_token = "2/ktZPikCIG3ZL076jYYNfe3mAg5TQ55Uz/GoA6dILTTUClIvRILXa/o96Lp8gfcLH5q2dxJeVtiHq8NZ5mNCHtH8VBUKzKfNBxIpMKZnn1XFCTFp/pTOwydGaGPvP0eFvZDsizHoFIRbWFTqYowyQdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
-//$myfile = "testt.txt";
-//$file = fopen($myfile,'r+')or die ("can't open file");
+$myfile = "testt.txt";
+$file = fopen($myfile,'r+')or die ("can't open file");
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -20,8 +20,8 @@ if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 // Get text sent
 $text = $event['source']['userId'];
 // Get replyToken
-  ///fwrite($file , var _export($text,true));
- // fclose($file);
+  fwrite($file , var_export($text,true));
+  fclose($file);
 $replyToken = $event['replyToken'];
 // Build message to reply back
 $messages = [
