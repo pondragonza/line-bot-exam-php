3d5637c549ca3323c8b5969fc5b46cddf7832f3c
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-18 17:53:08 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -5,7 +5,7 @@ require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 $access_token = '2/ktZPikCIG3ZL076jYYNfe3mAg5TQ55Uz/GoA6dILTTUClIvRILXa/o96Lp8gfcLH5q2dxJeVtiHq8NZ5mNCHtH8VBUKzKfNBxIpMKZnn1XFCTFp/pTOwydGaGPvP0eFvZDsizHoFIRbWFTqYowyQdB04t89/1O/w1cDnyilFU=';
 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 $myfile = "testt.txt";
-$file = fopen($myfile,'r+')or die ("can't open file");
+$file = fopen($myfile,'a')or die ("can't open file");
 $pond = 0;
 // Get POST body content
 $content = file_get_contents('php://input');
