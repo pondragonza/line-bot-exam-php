9946f7f21f474a3fb884e8db4154714e3694a33e
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-18 13:13:30 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -1,15 +1,25 @@
 <?php
 $proxy = 'proxyurl:port';
 $proxyauth = 'username:password';
+
+
 require "vendor/autoload.php";
+
 $access_token = '2/ktZPikCIG3ZL076jYYNfe3mAg5TQ55Uz/GoA6dILTTUClIvRILXa/o96Lp8gfcLH5q2dxJeVtiHq8NZ5mNCHtH8VBUKzKfNBxIpMKZnn1XFCTFp/pTOwydGaGPvP0eFvZDsizHoFIRbWFTqYowyQdB04t89/1O/w1cDnyilFU=';
+
 $channelSecret = '6d55c36fd534e5266875bc21a5674292';
+
 $pushID = 'U440df8f5d0462b99d170bf9effbcd26a';
+
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
+
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
 
 
 
