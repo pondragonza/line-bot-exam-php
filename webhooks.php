2fb7ad1dc2f8f288276089942b5de9ea6186fa88
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-19 13:02:00 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -92,7 +92,7 @@ if (!is_null($events['events'])) {
          }
 }
 //ของ
- // Loop through each event
+if (!is_null($events['events'])) {
           foreach ($events['events'] as $event) {
                // Reply only when message sent is in 'text' format
                if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
