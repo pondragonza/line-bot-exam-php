d84ed637ffcda1372dfe17be2f2d9516e43f5dee
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-19 11:26:02 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,6 +1,6 @@
 <?php
 // $data = file_get_contents("testt.txt");
 // $data1 = json_decode($data,true);
-$json = json_decode(file_get_contents("test.txt"), true);
+$json = json_decode(file_get_contents("testt.txt"), true);
 print_r($json);
 ?>
