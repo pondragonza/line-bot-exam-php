b166b7c5f8b769fc9f096f2c451d2faf6704ec05
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-19 14:17:43 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -95,7 +95,7 @@ if (!is_null($events['events'])) {
                       $oud = $event['message']['text']; 
                       $name = file_get_contents('name.txt');
                       $name2 = json_decode($name);
-                      $name3 = $name2['value'][0];
+                      
                       $file1 = fopen($myfile1,'r+')or die ("can't open file");
                       fwrite($file1 , print_r($name3,true));
                       fclose($file1);
