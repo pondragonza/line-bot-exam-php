cd2be2c37f3dd8167e693237ba0d5b1cd6d3404b
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-19 14:50:58 +0700

    Update o.php

--- o.php
+++ o.php
@@ -1,5 +1,7 @@
 <?php
-$myArr = array("John", "Mary", "Peter", "Sally");
-//$myJSON = json_encode($myArr[0]);
-echo $myArr[0];
+                      $name = file_get_contents('name.txt');
+                      $name2 = json_decode($name);
+// $myArr = array("John", "Mary", "Peter", "Sally");
+// //$myJSON = json_encode($myArr[0]);
+// echo $myArr[0];
 ?>
