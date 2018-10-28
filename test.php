8436ac00be94dd168cbf5c99bb0ee5214b31f232
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-28 23:25:44 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,10 @@
 <?php 
+require "testt.txt";
+require "test.php";
+https://pondragonza098@gmail.com:t3bGWBKaPq6RaR:@https://fixie:bzgCCzMBFxQGvPd@velodrome.usefixie.com:80;
+require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
+$access_token = '2/ktZPikCIG3ZL076jYYNfe3mAg5TQ55Uz/GoA6dILTTUClIvRILXa/o96Lp8gfcLH5q2dxJeVtiHq8NZ5mNCHtH8VBUKzKfNBxIpMKZnn1XFCTFp/pTOwydGaGPvP0eFvZDsizHoFIRbWFTqYowyQdB04t89/1O/w1cDnyilFU=';
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 $ch1 = curl_init("https://api.line.me/v2/bot/profile/Uf6071f679024997b537b91a0c8330a20);"//.$text);
                       //fwrite($file , var_export($ch1,true));
                      
@@ -11,4 +17,5 @@ $ch1 = curl_init("https://api.line.me/v2/bot/profile/Uf6071f679024997b537b91a0c8
                       curl_setopt($ch1, CURLOPT_PROXYUSERPWD, $proxyauth);
                       $result1 = curl_exec($ch1);
                       curl_close($ch1);
-                      print_r($result1)
+                      print_r($result1);
+?>
