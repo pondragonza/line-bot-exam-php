a4f0be536d8a56a601b3d1632f90a584dcfe125e
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-29 00:05:17 +0700

    Update webhooks22.php

--- webhooks22.php
+++ webhooks22.php
@@ -11,34 +11,66 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['source']['userId'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-			echo $result . "\r\n";
-		}
+		  if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+                      $dataa = $event;   
+                      $oud = $event['message']['text']; 
+                      $name = file_get_contents('name.txt');
+                      $name2 = json_decode($name);
+                     for ($i=0;$i<=1;$i++){ 
+                              if($oud == $name2[$i]) {
+                                $replyToken = $event['replyToken'];
+                                $messages = [
+                                'type' => 'text',
+                                'text' => 'เริ่มการทำงานของ line notify'
+                                ];
+                                $url = 'https://api.line.me/v2/bot/message/reply';
+                                $data = [
+                                'replyToken' => $replyToken,
+                                'messages' => [$messages],
+                                ];
+                                echo ($data);
+                                $post = json_encode($data);
+                                echo($post);
+                                $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+                                $ch = curl_init($url);
+                                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                                curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                                $result = curl_exec($ch);
+                                curl_close($ch);
+                                echo $result . "\r\n";
+                                $check = 1;
+                                }
+                   }
+                   if ($check == 0){
+                                $replyToken = $event['replyToken'];
+                                $messages = [
+                                'type' => 'text',
+                                'text' => 'หยุดการทำงาน line notify'
+                                ];
+                                $url = 'https://api.line.me/v2/bot/message/reply';
+                                $data = [
+                                'replyToken' => $replyToken,
+                                'messages' => [$messages],
+                                ];
+                                 echo($data);
+                                $post = json_encode($data);
+                                 echo($post);
+                                $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+                                $ch = curl_init($url);
+                                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                                curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                                $result = curl_exec($ch);
+                                curl_close($ch);
+                                echo $result . "\r\n";
+                                $check = 1;
+                   }          
+              }
 	}
 }
 echo "OK";
