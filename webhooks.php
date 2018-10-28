015f5200cc563e82aa0aa8061d8a0d7fb1d89c1b
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-28 22:53:34 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -7,6 +7,7 @@
 
 <?php // callback.php
 require "testt.txt";
+#require "test.php:;
 https://pondragonza098@gmail.com:t3bGWBKaPq6RaR:@https://fixie:bzgCCzMBFxQGvPd@velodrome.usefixie.com:80;
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 $access_token = '2/ktZPikCIG3ZL076jYYNfe3mAg5TQ55Uz/GoA6dILTTUClIvRILXa/o96Lp8gfcLH5q2dxJeVtiHq8NZ5mNCHtH8VBUKzKfNBxIpMKZnn1XFCTFp/pTOwydGaGPvP0eFvZDsizHoFIRbWFTqYowyQdB04t89/1O/w1cDnyilFU=';
