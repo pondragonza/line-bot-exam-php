89a27857c0f08f770b28a84b87fb4c04b6938519
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-28 16:50:51 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -125,7 +125,7 @@ if (!is_null($events['events'])) {
                                 $result = curl_exec($ch);
                                 curl_close($ch);
                                 echo $result . "\r\n";
-                                $check = 1;
+                                $check = 0;
 
                                 }
                    }
