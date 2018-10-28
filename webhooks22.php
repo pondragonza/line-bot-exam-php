3a50d016287e9a9c82d11551e238ebcddf23153b
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-29 00:41:49 +0700

    Update webhooks22.php

--- webhooks22.php
+++ webhooks22.php
@@ -1,16 +1,9 @@
 <?php // callback.php
-require "testt.txt";
-require "test.php";
 require "vendor/autoload.php";
-https://pondragonza098@gmail.com:t3bGWBKaPq6RaR:@https://fixie:bzgCCzMBFxQGvPd@velodrome.usefixie.com:80;
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 $access_token = '2/ktZPikCIG3ZL076jYYNfe3mAg5TQ55Uz/GoA6dILTTUClIvRILXa/o96Lp8gfcLH5q2dxJeVtiHq8NZ5mNCHtH8VBUKzKfNBxIpMKZnn1XFCTFp/pTOwydGaGPvP0eFvZDsizHoFIRbWFTqYowyQdB04t89/1O/w1cDnyilFU=';
+// Get POST body content
 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-$myfile = "testt.txt";
-$myfile1 = "mess.txt";
-$pond = 0;
-$check = 0;
-
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
@@ -32,12 +25,12 @@ if (!is_null($events['events'])) {
                       curl_setopt($ch1, CURLOPT_CUSTOMREQUEST, "GET");
                       curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
                      // curl_setopt($ch, CURLOPT_POSTFIELDS, $get);
-                      //curl_setopt($ch1, CURLOPT_HTTPHEADER, $headers);
+                      curl_setopt($ch1, CURLOPT_HTTPHEADER, $headers);
                       curl_setopt($ch1, CURLOPT_FOLLOWLOCATION, 1);
                       $result1 = curl_exec($ch1);
                       curl_close($ch1);
-                      $file = fopen($myfile,'w+')or die ("can't open file");
-                      fwrite($file , print_r($result1,true));
+                  //    $file = fopen($myfile,'w+')or die ("can't open file");
+                   //   fwrite($file , print_r($result1,true));
        //               fclose($file);
                       
 //                       fwrite($file1 , print_r($result1,true));
@@ -75,9 +68,9 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		  if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
                       $dataa = $event;
-                      $file1 = fopen($myfile1,'w+')or die ("can't open file");
-                      fwrite($file1 , print_r(json_encode($dataa),true));
-                      fclose($file1);   
+                    //  $file1 = fopen($myfile1,'w+')or die ("can't open file");
+                     // fwrite($file1 , print_r(json_encode($dataa),true));
+                     // fclose($file1);   
                       $oud = $event['message']['text']; 
                       $name = file_get_contents('name.txt');
                       $name2 = json_decode($name);
