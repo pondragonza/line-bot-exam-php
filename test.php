f71262fe60b9ab73b15cdfe3a1658d311b245e35
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-28 22:42:09 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,4 @@
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('2/ktZPikCIG3ZL076jYYNfe3mAg5TQ55Uz/GoA6dILTTUClIvRILXa/o96Lp8gfcLH5q2dxJeVtiHq8NZ5mNCHtH8VBUKzKfNBxIpMKZnn1XFCTFp/pTOwydGaGPvP0eFvZDsizHoFIRbWFTqYowyQdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
