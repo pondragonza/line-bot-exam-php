126898d39c36eb49af0825743bc5c41ad4dfe5c6
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-29 00:09:25 +0700

    Update webhooks22.php

--- webhooks22.php
+++ webhooks22.php
@@ -10,6 +10,60 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
+		 if ($event['type'] == 'follow' ) {
+                       // Get text sent
+                      $text = $event['source']['userId']; 
+         //             $file = fopen($myfile,'w+')or die ("can't open file");
+         //             fwrite($file , print_r($text,true));
+                         $text1 = array('userId'=>$text);
+                             
+                      
+                      $ch1 = curl_init("https://api.line.me/v2/bot/profile/Uf6071f679024997b537b91a0c8330a20);"//.$text);
+                      //fwrite($file , var_export($ch1,true));
+                     
+                      curl_setopt($ch1, CURLOPT_CUSTOMREQUEST, "GET");
+                      curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
+                      //curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                      curl_setopt($ch1, CURLOPT_HTTPHEADER, $headers);
+                      curl_setopt($ch1, CURLOPT_FOLLOWLOCATION, 1);
+                      $result1 = curl_exec($ch1);
+                      curl_close($ch1);
+                     // $file = fopen($myfile,'w+')or die ("can't open file");
+                   //   fwrite($file , print_r($result1,true));
+       //               fclose($file);
+                      
+//                       fwrite($file1 , print_r($result1,true));
+//                       fclose($file1);
+                       $events2 = json_decode($result1, true);
+                       $a = $events2['displayName'];
+                       $b = ("Hello ".$a." พิมพ์ 1 เพื่อเรียกใช้ฟังก์ชั่น line notify");
+                       $replyToken = $event['replyToken'];
+                      // Build message to reply back
+                      $messages = [
+                      'type' => 'text',
+                      'text' => $b
+                      ];
+                      // Make a POST Request to Messaging API to reply to sender
+                      $url = 'https://api.line.me/v2/bot/message/reply';
+                      $data = [
+                      'replyToken' => $replyToken,
+                      'messages' => [$messages],
+                      ];
+                      echo ($data);
+                      $post = json_encode($data);
+                      echo($post);
+                      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+                      $ch = curl_init($url);
+                      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                      $result = curl_exec($ch);
+                      curl_close($ch);
+                      echo $result . "\r\n";
+                      
+                }
 		// Reply only when message sent is in 'text' format
 		  if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
                       $dataa = $event;   
