14e1fa2c2858c0dd16e48b74d1a46366b182f649
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-28 23:13:32 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,15 @@
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<2/ktZPikCIG3ZL076jYYNfe3mAg5TQ55Uz/GoA6dILTTUClIvRILXa/o96Lp8gfcLH5q2dxJeVtiHq8NZ5mNCHtH8VBUKzKfNBxIpMKZnn1XFCTFp/pTOwydGaGPvP0eFvZDsizHoFIRbWFTqYowyQdB04t89/1O/w1cDnyilFU=>');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<6d55c36fd534e5266875bc21a5674292>']);
-
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->replyMessage('<5d03e92c56634888bf818f01127d5618>', $textMessageBuilder);
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+   $ch1 = curl_init("https://api.line.me/v2/bot/profile/Uf6071f679024997b537b91a0c8330a20);"//.$text);
+                      //fwrite($file , var_export($ch1,true));
+                     
+                      curl_setopt($ch1, CURLOPT_CUSTOMREQUEST, "GET");
+                      curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
+                      //curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                      curl_setopt($ch1, CURLOPT_HTTPHEADER, $headers);
+                      curl_setopt($ch1, CURLOPT_FOLLOWLOCATION, 1);
+                      curl_setopt($ch1, CURLOPT_PROXY, $proxy);
+                      curl_setopt($ch1, CURLOPT_PROXYUSERPWD, $proxyauth);
+                      $result1 = curl_exec($ch1);
+                      curl_close($ch1);
+                      $file = fopen($myfile,'w+')or die ("can't open file");
+                      fwrite($file , print_r($result1,true));
+                      print_r($result1)
