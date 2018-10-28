5e468575f857c6bfdb540b75093060fbec337e82
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-28 22:31:19 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -9,7 +9,7 @@
 require "testt.txt";
 https://pondragonza098@gmail.com:t3bGWBKaPq6RaR:@https://fixie:bzgCCzMBFxQGvPd@velodrome.usefixie.com:80;
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-$access_token = 'v8ZeLFUyG6JfMw7rYvZybHnI89Q8JABiucAvJ3SU1F/QBkGmt2G30TOENwICIhcmBkHFQ0nbiQLpuYPsmiwRKUXxxJWO/nqWUb+Dm+lmBXKhN6X3z2UTu+xOoGDmC5WeCDQ0zPRLUV/DwE6rmly26AdB04t89/1O/w1cDnyilFU=';
+$access_token = '2/ktZPikCIG3ZL076jYYNfe3mAg5TQ55Uz/GoA6dILTTUClIvRILXa/o96Lp8gfcLH5q2dxJeVtiHq8NZ5mNCHtH8VBUKzKfNBxIpMKZnn1XFCTFp/pTOwydGaGPvP0eFvZDsizHoFIRbWFTqYowyQdB04t89/1O/w1cDnyilFU=';
 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 $myfile = "testt.txt";
 $myfile1 = "mess.txt";
