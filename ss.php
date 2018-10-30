5759014c8377eb6c03f84ce43092923d7bd7cd1d
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-30 17:18:51 +0700

    Update ss.php

--- ss.php
+++ ss.php
@@ -3,10 +3,15 @@
 // $myfile = "value.txt";
 // $fileHandle = fopen($myfile,'w+')or die("can't open file");
 $a = ($_POST["hum"]);
+print_r($a);
 $b = ($_POST["temp"]);
+print_r($b);
 $c = ($_POST["status"]);
+print_r($c);
 $d = ($_POST["startTime"]);
+print_r($d);
 $e = ($_POST["endTime"]);
+print_r($e);
 
 //print_r($a);
 // print_r($b);
