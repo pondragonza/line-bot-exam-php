3bc1d0f3073bbc57c1940a73c6acf855d389e03a
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-30 17:38:36 +0700

    Update ss.php

--- ss.php
+++ ss.php
@@ -1,33 +1,42 @@
 <?php
-// require "value.txt" ;
-// $myfile = "value.txt";
-// $fileHandle = fopen($myfile,'w+')or die("can't open file");
-$c = ($_POST["status"]);
-print_r($c);
+require "value.txt" ;
+$myfile = "value.txt";
+$fileHandle = fopen($myfile,'w+')or die("can't open file");
 $a = ($_POST["hum"]);
 // print_r($a);
 $b = ($_POST["temp"]);
 // print_r($b);
-// $c = ($_POST["status"]);
+$c = ($_POST["status"]);
 // print_r($c);
-//$d = ($_POST["startTime"]);
+$d = ($_POST["startTime"]);
 // print_r($d);
-// $e = ($_POST["endTime"]);
+$e = ($_POST["endTime"]);
 // print_r($e);
 
 //print_r($a);
 // print_r($b);
- $output= '{
-"value": [
-{"temp": "'.$b.'"},
-{"hum": "'.$a.'"},
-{"status":"'.$c.'"},
-{"startTime":"'.$d.'"},
-{"endTime":"'.$e.'"}
-]
-}';
+if $c == "true"{
+    $output= '{
+   "value": [
+   {"temp": "'.$b.'"},
+   {"hum": "'.$a.'"},
+   {"status":"'.$c.'"}
+   ]
+   }';
+}
+else {
+     $output= '{
+    "value": [
+    {"temp": "'.$b.'"},
+    {"hum": "'.$a.'"},
+    {"status":"'.$c.'"},
+    {"startTime":"'.$d.'"},
+    {"endTime":"'.$e.'"}
+    ]
+    }';
+}
 // print_r($output);
-//fwrite($fileHandle, $a);
-// fwrite($fileHandle, print_r($output, true));
-//   fclose($fileHandle);
+// fwrite($fileHandle, $a);
+fwrite($fileHandle, print_r($output, true));
+  fclose($fileHandle);
 ?>
