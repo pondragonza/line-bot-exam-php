c456cead929036756a28297d056b520fb9afb339
pondragonza <43951902+pondragonza@users.noreply.github.com>
2018-10-31 10:51:28 +0700

    Update oud.php

--- oud.php
+++ oud.php
@@ -1,5 +1,5 @@
-<?php
-$content = file_get_contents('value.txt');
-$d = (json_decode($content));
-print_r($d[0]);
-?>
+<!-- <?php
+// $content = file_get_contents('value.txt');
+// $d = (json_decode($content));
+// print_r($d[0]);
+?> -->
