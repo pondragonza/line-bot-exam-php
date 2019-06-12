2405756f6fb76f4a53ca2a7bff061b5234408b94
pondragonza <43951902+pondragonza@users.noreply.github.com>
2019-06-12 14:20:05 +0700

    Update publish.php

--- examples/publish.php
+++ examples/publish.php
@@ -2,16 +2,17 @@
 
 require("../phpMQTT.php");
 
-$server = "mqtt.example.com";     // change if necessary
+$server = "tcp://10.0.20.1";     // change if necessary
 $port = 1883;                     // change if necessary
-$username = "";                   // set your username
-$password = "";                   // set your password
+$username = "carparkmqtt";                   // set your username
+$password = "carparkmqtt";                   // set your password
 $client_id = "phpMQTT-publisher"; // make sure this is unique for connecting to sever - you could use uniqid()
 
 $mqtt = new phpMQTT($server, $port, $client_id);
 
 if ($mqtt->connect(true, NULL, $username, $password)) {
-	$mqtt->publish("bluerhinos/phpMQTT/examples/publishtest", "Hello World! at " . date("r"), 0);
+	//$mqtt->publish("bluerhinos/phpMQTT/examples/publishtest", "Hello World! at " . date("r"), 0);
+	$mqtt->publish("/phpMQTT/examples/publishtest", "Hello World! at " . date("r"), 0);
 	$mqtt->close();
 } else {
     echo "Time out!\n";
