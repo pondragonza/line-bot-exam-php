42bf96ff98b60721799067b07c6e56230d63e179
pondragonza <43951902+pondragonza@users.noreply.github.com>
2019-06-12 15:27:53 +0700

    Update publish.php

--- test/examples/examples/publish.php
+++ test/examples/examples/publish.php
@@ -1,6 +1,7 @@
 <?php
 
-require("test/phpMQTT.php");
+//require("test/phpMQTT.php");
+require("../phpMQTT.php");
 
 $server = "tcp://10.0.20.1";     // change if necessary
 $port = 1883;                     // change if necessary
