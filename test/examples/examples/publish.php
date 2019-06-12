99d7673ccf0241f23c22226750e111d57ed2c2fd
pondragonza <43951902+pondragonza@users.noreply.github.com>
2019-06-12 15:25:33 +0700

    Update publish.php

--- test/examples/examples/publish.php
+++ test/examples/examples/publish.php
@@ -11,7 +11,7 @@ $client_id = "phpMQTT-publisher"; // make sure this is unique for connecting to 
 $mqtt = new phpMQTT($server, $port, $client_id);
 
 if ($mqtt->connect(true, NULL, $username, $password)) {
-	$mqtt->publish("bluerhinos/phpMQTT/examples/publishtest", "Hello World! at " . date("r"), 0);
+	$mqtt->publish("temp", "Hello World! at " . date("r"), 0);
 	$mqtt->close();
 } else {
     echo "Time out!\n";
